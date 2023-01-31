fc035439576b8d679a3a5f8b41270f8260db0b05
savik7 <viktor.savchenko00@gmail.com>
2023-01-31 13:47:16 +0300

    Рабочая игрушка светофор

--- app/src/main/java/com/savik/semafor/MainActivity.kt
+++ app/src/main/java/com/savik/semafor/MainActivity.kt
@@ -1,11 +1,54 @@
 package com.savik.semafor
 
-import androidx.appcompat.app.AppCompatActivity
+import android.app.Activity
 import android.os.Bundle
+import android.view.View
+import android.widget.ImageButton
+import android.widget.ImageView
+import java.util.Timer
+import java.util.TimerTask
 
-class MainActivity : AppCompatActivity() {
+class MainActivity : Activity() {
+    var imSemafor:ImageView? = null
+    var counter:Int = 0
+    var timer:Timer? = null
+    var is_run = false
+    var imArray:IntArray = intArrayOf(R.drawable.semafor_red, R.drawable.semafor_ell, R.drawable.semafor_green)
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        imSemafor = findViewById(R.id.imSemafor)
+    }
+
+    fun onClickStartStop(view:View) {
+        view as ImageButton
+        if (!is_run){
+            startStop()
+            view.setImageResource(R.drawable.button_stop)
+            is_run = true
+        } else {
+            timer?.cancel()
+            view.setImageResource(R.drawable.button_start)
+            imSemafor?.setImageResource(R.drawable.semafor_grey)
+            is_run = false
+            counter = 0
+        }
+    }
+
+    fun startStop(){
+        timer = Timer()
+        timer?.schedule(object: TimerTask(){
+            override fun run() {
+                runOnUiThread(){
+                    imSemafor?.setImageResource(imArray[counter])
+                    counter++
+                    if (counter == 3){
+                        counter = 0
+                    }
+                }
+            }
+        }, 0, 1000)
     }
 }
